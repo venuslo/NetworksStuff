6a2567da6c36f610c60df8d6c8b7edab5f95beec
Venus <vhl8@cornell.edu>
2017-02-10 17:55:31 -0500

    I think the reading is correct now

--- tryRead.py
+++ tryRead.py
@@ -1,3 +1,17 @@
 f=open('HW1_data.txt', 'r')
-s=f.readline().strip().split("  ")
+s=f.readline().strip().split(",")[0] #takes out everything after authors
+
+s= s.split("&") #takes out all subsequent authors; first entry is year up to first author
+
 print s
+
+
+initial = s[0]
+initial = initial.split(" ") #split the first entry into years, etc.
+n = len(initial)
+s[0] = initial[n-1]
+
+year = int(initial[0])
+
+print s
+print year
