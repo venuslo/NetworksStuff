070dc40c10bd240fa68df92e02ef7ce270b7e347
Venus <vhl8@cornell.edu>
2017-02-12 17:15:00 -0500

    can read from file properly

--- HW1.py
+++ HW1.py
@@ -20,23 +20,62 @@ class node:
 
 class paper:
 	def __init__(self, traits):
-		(year, number, conference, authors) = traits
-		self.year = int(year)
-		self.number = int(number)
-		self.conference = conference
+		(year, authors, title) = traits
+		self.year = year
 		self.authors = authors
+		self.title = title
 
 
 
+def readPapers(g):
+	listOfPaper = []
 
+	while True:
+		try:
+			print('ok')
+			line = g.readline().strip().split(",")
+			print line
+			title = line[1]
+		
+			line = line[0].strip("(Ed.)").split("&")  #after removing author
+			m = len(line)
+
+			authors = []
+			for i in range(1, m): #add authors 2 onwards
+				authors.append(line[i].strip())
+
+			line = line[0].strip().split(" ") #(year, [num], conf, 1st author)
+
+			m = len(line)
+			authors.append(line[m-1])
+		
+
+			year = int(line[0]) #num and conf ignored for now
+
+			x = paper((year, authors, title))
+			listOfPaper.append(x)
+
+		except:
+			break
+
+	return listOfPaper		
+	
 
 
 
 #main
   
-if __name__ == `__main__':
+if __name__ == "__main__":
 
-	f = open("hw1solution.txt")
+	f = open("hw1solution.txt", 'w')
 	f.write("vhl8" + "\n")
 
+	paperF = open("HW1_data.txt", "r")
+	listOfPaper = readPapers(paperF)
+	print len(listOfPaper)
+	listOfPaper = filter(lambda x: 1985<= x.year <=2005, listOfPaper)
+
 	 
+
+	f.close
+	paperF.close	 
